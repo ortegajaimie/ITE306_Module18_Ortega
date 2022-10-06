d8c46cb3159e31fa12fad454a9bc1057b51da93f
Jaimie C. Ortega <115129929+ortegajaimie@users.noreply.github.com>
2022-10-06 12:44:51 +0800

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(_name_)
+empDB = [
+    {
+        'id': '101',
+        'name': 'Dorry Britz',
+        'title': 'Technical Leader',
+    },
+    {
+        'id': '102',
+        'name': 'Ice',
+        'title': 'Software Engineer',
+    }
+]
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route('/empdb/employee/', methods=['GET'])
+def getAllEmp():
+    return jsonify({'emp': empDB})
+
+
+@app.route('/empdb/employee/<empId>', methods=['GET'])
+def getEMP(empId):
+    usr = [emp for emp in empDB if (emp['id'] == empId)]
+    return jsonify({'emp': usr})
+
+
+if _name_ == "_main_":
+    app.run()
